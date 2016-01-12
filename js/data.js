2b5e5e6afd7f09b73830384d0175099105b60ee6
Joe Hos <joehoskisson@gmail.com>
2016-01-12 14:04:28 -0700

    Added data.js

---
+++ js/data.js
@@ -0,0 +1,396 @@
+var treeData = {
+	bigBr: {
+		1: {
+			name: "Science",
+			medBr: {
+				1: {
+					name: "Geeeemology",
+					smBr: {
+						1: {
+							name: "Rocks"
+						},
+						2: {
+							name: "Lava"
+						},
+						3: {
+							name: "Sediment"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Geeeeology.com"
+						}
+					}
+				},
+				2: {
+					name: "Physics",
+					smBr: {
+						1: {
+							name: "Classical Mechanics"
+						},
+						2: {
+							name: "Electricity & Magnetism"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Pysics.com"
+						}
+					}
+				},
+				3: {
+					name: "Biology",
+					smBr: {
+						1: {
+							name: "Microbiology"
+						},
+						2: {
+							name: "Animal Kingdom"
+						},
+						3: {
+							name: "Zoooology"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Biology.com"
+						}
+					}
+				},
+				4: {
+					name: "Chemistry",
+					smBr: {
+						1: {
+							name: "General Chemistry"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Chemistry.com"
+						} //end 1
+					} //end leaf
+				}//end 4 (medBr)
+			}//end medBr
+		},//end 1 (bigBr)
+		2: { //start bigBr2
+			name: "Technology",
+			medBr: {
+				1: {
+					name: "Computer Science",
+					smBr: {
+						1: {
+							name: "Programming"
+						},
+						2: {
+							name: "Networks"
+						},
+						3: {
+							name: "Hardware"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Geeeeology.com"
+						}
+					}
+				},
+				2: {
+					name: "Information",
+					smBr: {
+						1: {
+							name: "Internet"
+						},
+						2: {
+							name: "Books"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Pysics.com"
+						}
+					}
+				},
+				3: {
+					name: "Aeronautical",
+					smBr: {
+						1: {
+							name: "Space"
+						},
+						2: {
+							name: "In-Earth Flight"
+						},
+						3: {
+							name: "Future of Aeronautical"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Biology.com"
+						}
+					}
+				},
+				4: {
+					name: "Manufacturing",
+					smBr: {
+						1: {
+							name: "Textiles"
+						}
+					},
+					leaf: {
+						1: {
+							name: "whois.com"
+						}
+					}
+				}//end 4 (medBr)
+			}//end medBr
+		},//end 2 (bigBr)
+		3: { //start bigBr3
+			name: "Engineering",
+			medBr: {
+				1: {
+					name: "Mechanical Engineering",
+					smBr: {
+						1: {
+							name: "Structures"
+						},
+						2: {
+							name: "Mechanics"
+						},
+						3: {
+							name: "Design"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Geeeeology.com"
+						}
+					}
+				},
+				2: {
+					name: "Civil Engineering",
+					smBr: {
+						1: {
+							name: "Structures"
+						},
+						2: {
+							name: "Water"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Pysics.com"
+						}
+					}
+				},
+				3: {
+					name: "Chemical Engineering",
+					smBr: {
+						1: {
+							name: "Chemistry"
+						},
+						2: {
+							name: "Processes"
+						},
+						3: {
+							name: "Unit Operations"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Biology.com"
+						}
+					}
+				},
+				4: {
+					name: "Electrical Engineering",
+					smBr: {
+						1: {
+							name: "Circuits"
+						}
+					},
+					leaf: {
+						1: {
+							name: "whois.com"
+						}
+					}
+				}//end 4 (medBr)
+			}//end medBr
+		},//end 3 (bigBr)
+		4: { //start bigBr4
+			name: "Math",
+			medBr: {
+				1: {
+					name: "Arithmetic",
+					smBr: {
+						1: {
+							name: "Addition and Subtraction"
+						},
+						2: {
+							name: "Multiplication and Division"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Geeeeology.com"
+						}
+					}
+				},
+				2: {
+					name: "Algebra 1",
+					smBr: {
+						1: {
+							name: "Negative Numbers"
+						},
+						2: {
+							name: "Solving Equations"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Pysics.com"
+						}
+					}
+				},
+				3: {
+					name: "Geometry",
+					smBr: {
+						1: {
+							name: "Area"
+						},
+						2: {
+							name: "Angles"
+						},
+						3: {
+							name: "Aquaducts---what?"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Biology.com"
+						}
+					}
+				},
+				4: {
+					name: "Fun Math",
+					smBr: {
+						1: {
+							name: "Math Jokes"
+						},
+						2: {
+							name: "Jokes Math"
+						}
+					},
+					leaf: {
+						1: {
+							name: "whois.com"
+						}
+					}
+				},//end 4 (medBr)
+				5: {
+					name: "Pre Algebra",
+					smBr: {
+						1: {
+							name: "Fractions"
+						}
+					},
+					leaf: {
+						1: {
+							name: "whois.com"
+						}
+					}
+				}//end 5 (medBr)
+			}//end medBr
+		},//end 4 (bigBr)
+		5: { //start bigBr5
+			name: "Art",
+			medBr: {
+				1: {
+					name: "Elements of Art",
+					smBr: {
+						1: {
+							name: "Line"
+						},
+						2: {
+							name: "Color"
+						},
+						3: {
+							name: "Shape"
+						},
+						4: {
+							name: "Form"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Geeeeology.com"
+						}
+					}
+				},
+				2: {
+					name: "Sculpture",
+					smBr: {
+						1: {
+							name: "Clay"
+						},
+						2: {
+							name: "Wood"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Pysics.com"
+						}
+					}
+				},
+				3: {
+					name: "Painting & Drawing",
+					smBr: {
+						1: {
+							name: "Water Color"
+						},
+						2: {
+							name: "Oil"
+						},
+						3: {
+							name: "Charcoal?"
+						}
+					},
+					leaf: {
+						1: {
+							name: "Biology.com"
+						}
+					}
+				},
+				4: {
+					name: "Dance",
+					smBr: {
+						1: {
+							name: "Contemporary"
+						},
+						2: {
+							name: "Classical"
+						}
+					},
+					leaf: {
+						1: {
+							name: "whois.com"
+						}
+					}
+				},//end 4 (medBr)
+				5: {
+					name: "Music",
+					smBr: {
+						1: {
+							name: "Contemporary"
+						}
+					},
+					leaf: {
+						1: {
+							name: "whois.com"
+						}
+					}
+				}//end 5 (medBr)
+			}//end medBr
+		}//end 5 (bigBr)
+	}//end bigBr
+};//end treeData
