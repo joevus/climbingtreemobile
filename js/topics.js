a006b73e670a49f43710de7edcdeee48539e46f5
Joe Hos <joehoskisson@gmail.com>
2016-01-27 14:27:33 -0700

    put in a container, row and col, changed some css. chaos.

--- js/topics.js
+++ js/topics.js
@@ -93,7 +93,7 @@ function middleTopics() {
 		var txt = treePos[i+1].name;
 		var txtNode= document.createTextNode(txt);
 		brs[i].appendChild(txtNode);
-		$(brs[i]).appendTo('.wrap1');
+		$(brs[i]).appendTo('.row');
 		console.log("middleTopics i: " + i);
 		/* store branch location data.
 		*  For gen:
